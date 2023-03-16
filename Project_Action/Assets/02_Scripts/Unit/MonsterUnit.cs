72129b702edc83ba0587435f6b0e5a92f114f3b8
ParkWonGyu <dsfa122@gmail.com>
2023-03-16 17:03:31 +0900

    몬스터 자동 이동(패트롤) 로직 작성

--- Project_Action/Assets/02_Scripts/Unit/MonsterUnit.cs
+++ Project_Action/Assets/02_Scripts/Unit/MonsterUnit.cs
@@ -6,20 +6,39 @@ public class MonsterUnit : Unit
 {
     public bool battle = false;
     public PlayerUnit player;
+    public Transform monsterTr;
     public Vector3 tr;
+    public Vector3 velocity;
 
     public void Start()
     {
+        tr = monsterTr.position;
         StartCoroutine(MonsterAI());
     }
     IEnumerator MonsterAI()
     {
         float t = 0;
 
-        while(!battle)
+        Vector3 newPos = new Vector3(tr.x + Random.Range(-5, 5), tr.y, tr.z + Random.Range(-5, 5));
+        float distance = Vector3.Distance(monsterTr.position, newPos);
+        Vector3 dir = newPos - monsterTr.position;
+        dir.y = 0;
+        while (!battle)
         {
             t += Time.deltaTime;
-            Vector3 newPos = new Vector3(tr.x + Random.Range(-3, 3), tr.y, tr.z + Random.Range(-3, 3));
+
+            if (t > 5f)
+            {
+                t = 0;
+                newPos = new Vector3(tr.x + Random.Range(-5, 5), tr.y, tr.z + Random.Range(-5, 5));
+                distance = Vector3.Distance(monsterTr.position, newPos);
+                dir = newPos - monsterTr.position;
+                dir.y = 0;
+                Move();
+            }
+
+            monsterTr.rotation = Quaternion.Lerp(transform.rotation , Quaternion.LookRotation(dir), 0.2f);
+            monsterTr.position = Vector3.SmoothDamp(monsterTr.position, newPos, ref velocity, distance * 0.5f);
 
             yield return null;
         }
